ef29deddf9cf2e9cfee5a32a2223233f1b824eb4
Gema Vidal <gemavidal@Gemas-MacBook-Pro.local>
2022-01-18 15:51:35 +0100

    Commit updates.

--- R/toyFunction.R
+++ R/toyFunction.R
@@ -1,4 +1,16 @@
 # Toy function
+
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alpha,bravo,charlie,delta"
+#' strsplit(x, split = ",)
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
